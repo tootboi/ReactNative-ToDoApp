77e983559f127ea93c0fc1b3e44b4ba497a4ded7
Daniel Yim <55238464+tootboi@users.noreply.github.com>
2020-06-29 18:41:13 +1200

    added icon for deleting todos

--- components/todoItem.js
+++ components/todoItem.js
@@ -1,21 +1,36 @@
 import React from 'react';
-import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Entypo } from '@expo/vector-icons';
 
 export default function TodoItem({ item, deleteTodo }) {
     return (
-        <TouchableOpacity onPress={() => deleteTodo(item.id)}>
-            <Text style={styles.item}>{item.text}</Text>
+        <TouchableOpacity style={styles.container}>
+            <View style={styles.item}>
+                <Text>{item.text}</Text>
+                <Entypo name='cross' size={18} color='#929292' style={styles.itemIcon} onPress={() => deleteTodo(item.id)}/>
+            </View> 
         </TouchableOpacity>
     );
 }
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
     item: {
+        flexDirection: 'row',
         padding: 16,
         marginTop: 16,
         borderColor: '#bbb',
         borderWidth: 1,
-        borderStyle: 'dashed',
+        borderStyle: 'solid',
         borderRadius: 10,
+    },
+    itemIcon: {
+        position: 'absolute',
+        right: 0,
+        top: 16,
+        width: 30,
+        height: 30
     }
 })
